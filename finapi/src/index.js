98c03db1fc21891bfe8371f118d39b3c472d11f2
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-10-24 20:39:47 -0300

    feat(finApi): cadastro de conta

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
+
+app.use(express.json())
+
+const constumers = []
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+  const id = uuidv4()
+
+  const customer = { id, cpf, name, statement: [] }
+
+  constumers.push(customer)
+
+  return response.status(201).send(customer)
+
+})
 
 app.listen(3333)
