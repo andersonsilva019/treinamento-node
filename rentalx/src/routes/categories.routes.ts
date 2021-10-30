c4804782429560057dae740e1671ef2f90a4107e
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-10-30 14:43:48 -0300

    feat: Inserindo ID com uuid

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,16 +1,21 @@
 import { Router } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuidv4(),
     name,
     description,
-  });
+    created_at: new Date(),
+  };
+
+  categories.push(category);
 
   return response.status(201).send();
 });
