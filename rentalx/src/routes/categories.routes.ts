b92b30e121071dc5a49da13f637b0c56e0457701
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-10-30 19:54:40 -0300

    feat: Listando as categorias

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 
-import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
@@ -13,6 +12,12 @@ categoriesRoutes.post('/', (request, response) => {
   categoryRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoryRepository.list();
+
+  return response.json(categories);
 });
 
 export { categoriesRoutes };
