e9dce518ed93dbc9a2baaf82d8a5ee7409cb6b69
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-11 19:37:51 -0300

    feat: criando controller de usuario

--- rentalx/src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ rentalx/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,1 +1,22 @@
-export class CreateUserController {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    await createUserUseCase.execute({
+      name,
+      email,
+      username,
+      password,
+      driver_license,
+    });
+
+    return response.status(201).send();
+  }
+}
