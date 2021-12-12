8b269391085759b6d15fb9007d866a333bee6503
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-11 21:44:01 -0300

    refactor: Tratamento de exceções

--- rentalx/src/modules/accounts/useCases/authenticationUser/AuthenticationUserUseCase.ts
+++ rentalx/src/modules/accounts/useCases/authenticationUser/AuthenticationUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { IAuthenticationUserDTO } from '../../dtos/IAuthenticationUserDTO';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
@@ -24,13 +25,13 @@ export class AuthenticationUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or password incorrect!');
+      throw new AppError('Email or password incorrect!');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or password incorrect!');
+      throw new AppError('Email or password incorrect!');
     }
 
     const token = sign({}, 'f420fc990cadffcae629e64bdf0bf7c2', {
