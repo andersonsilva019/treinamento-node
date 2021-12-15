2b3affb0cf55dd9215ca1bad1fb9af4462a1de77
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-12-15 13:15:36 -0300

    test: Teste de criação de categoria

--- rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,6 +1,51 @@
+import { AppError } from '../../../../errors/AppError';
+import { CategoriesRepositoryFake } from '../../repositories/fakes/CategoriesRepositoryFake';
+import { CreateCategoryUseCase } from './CreateCategoryUseCase';
+
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryFake: CategoriesRepositoryFake;
+
 describe('#CreateCategoryUseCase', () => {
-  it('should return sum two numbers', () => {
-    const sum = 2 + 2;
-    expect(sum).toBe(4);
+  beforeEach(() => {
+    categoriesRepositoryFake = new CategoriesRepositoryFake();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryFake);
+  });
+
+  it('should be able to create a new category', async () => {
+    const newCategory = {
+      name: 'Category name Test',
+      description: 'Category description Test',
+    };
+
+    await createCategoryUseCase.execute({
+      name: newCategory.name,
+      description: newCategory.description,
+    });
+
+    const category = await categoriesRepositoryFake.findByName(
+      newCategory.name
+    );
+
+    expect(category.name).toBe(newCategory.name);
+    expect(category).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new category with name exists', async () => {
+    expect(async () => {
+      const newCategory = {
+        name: 'Category name Test',
+        description: 'Category description Test',
+      };
+
+      await createCategoryUseCase.execute({
+        name: newCategory.name,
+        description: newCategory.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: newCategory.name,
+        description: newCategory.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
