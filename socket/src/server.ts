89bb0a5f2a4229a2de6a4d5567fae102f2d3b464
Anderson Silva <andersonnsilva@alu.ufc.br>
2022-05-21 15:46:08 -0300

    Configurando socket na aplicacao

--- socket/src/server.ts
+++ socket/src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import path from 'path';
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const app = express();
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+})
 
 app.get('/', (req, res) => {
   return res.send('Hello World!');
 })
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+server.listen(3000, () => console.log('Server started on port 3000'));
